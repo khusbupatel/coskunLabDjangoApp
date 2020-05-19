069995a448ea746ee0510acc84939ddd946c21ee
Khusbu Patel <khusbu0514@gmail.com>
2020-05-19 14:17:39 -0400

    created Order model and views and slack views

--- quickOrder/serializers.py
+++ quickOrder/serializers.py
@@ -1,10 +1,16 @@
 from rest_framework import serializers
 
 from .models import InventoryItem
+from .models import Order
 from django import forms
 
 # transformation from model to json
 class InventoryItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = InventoryItem
+        fields = '__all__'
+
+class OrderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Order
         fields = '__all__'
