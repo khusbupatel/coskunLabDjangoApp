accf0e44d7b67cec865816bcb8d6e0484a0a313e
Allison Fister <allisonfister59@gmail.com>
2020-06-26 15:03:43 -0400

    fixed typo

--- levelFeature/urls.py
+++ levelFeature/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('get_reading/<int:sr_num>/', views.get_reading, name='get_reading'),
     path('get_latest/', views.get_latest, name='get_latest'),
     path('post_reading/', views.post_reading, name='post_reading'),
-    path('delete_readings/', views.delete_readings, name='delete_reading')
+    path('delete_readings/', views.delete_readings, name='delete_readings')
 ]
